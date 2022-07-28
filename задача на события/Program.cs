1c419d91462053e7dc326812c85fb57a676369c6
bastis <bastis@TRYDEX-LAPTOP>
2022-07-28 17:54:32 +0300

    Add project files.

---
+++ задача на события/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+
+    public class EventUsing
+    {
+        class SingleFileManager
+        {
+            public string fileName;
+            public SingleFileManager(string fileName) => this.fileName = fileName;
+            public void Create()
+            {
+                if (!File.Exists(fileName)) File.Create(fileName);
+                Console.WriteLine("The file has been created");
+            }
+            public void AppendData(string data)
+            {
+                using (var fstream = new FileStream(fileName, FileMode.Open))
+                {
+                    
+                    Console.WriteLine("The file has been deleted");
+                }
+            }
+            public void Delete()
+            {
+                if (File.Exists(fileName)) File.Delete(fileName);
+            }
+
+        }
+        public void Main()
+        {
+            var fileManager = new SingleFileManager("newfile.txt");
+
+            fileManager.OnFileCreated += x => Console.WriteLine($"Файл {x.FileName} создан");
+            fileManager.OnFileDeleted += x => Console.WriteLine($"Файл {x.FileName} удален");
+            fileManager.OnFileModified += x => Console.WriteLine($"В файл {x.FileName} была добавлено {x.AppendData}");
+
+            fileManager.Create();
+            fileManager.AppendData("Новая инфа");
+            fileManager.Delete();
+        }
+    }
+}
