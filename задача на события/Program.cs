8af2064326aace2291f2e0f309a07e8350952ea6
Galina Amelina <galia.amelina@yandex.ru>
2022-07-31 20:41:59 +0300

    Added delegate and events

--- задача на события/Program.cs
+++ задача на события/Program.cs
@@ -5,56 +5,77 @@
     {
         class SingleFileManager
         {
-            public string fileName;
-            public SingleFileManager(string fileName) => this.fileName = fileName;
+            public string name;
+            public SingleFileManager(string fileName) => this.name = fileName;
+
+            public delegate void FileHandler(SingleFileManagerEventArgs e);
+            public event FileHandler? OnFileCreated;
+            public event FileHandler? OnFileDeleted;
+            public event FileHandler? OnFileModified;
+
+
             public void Create()
             {
-                using (var fstream = new FileStream(fileName, FileMode.OpenOrCreate))
+                using (var fstream = new FileStream(name, FileMode.OpenOrCreate))
                 {
-                    Console.WriteLine($"{fileName} has been created");
+                    OnFileCreated?.Invoke(new SingleFileManagerEventArgs($"{name} has been created", name, null));
                 }
             }
             public void AppendData(string data)
             {
 
-                if (File.Exists(fileName))
+                if (File.Exists(name))
                 {
-                    using (var writer = new StreamWriter(fileName, true))
+                    using (var writer = new StreamWriter(name, true))
                     {
                         writer.WriteLine(data);
-                        Console.WriteLine($"{fileName} has been changed");
+                        OnFileModified?.Invoke(new SingleFileManagerEventArgs($"{name} has been changed", name, data));
                     }
                 }
                 else
                 {
-                    Console.WriteLine($"There's no file named {fileName} to add new information");
+                    Console.WriteLine($"There's no file named {name} to add new information");
                 }
             }
             public void Delete()
             {
-                if (File.Exists(fileName))
+                if (File.Exists(name))
                 {
-                    File.Delete(fileName);
-                    Console.WriteLine($"{fileName} has been deleted");
+                    File.Delete(name);
+                    OnFileDeleted?.Invoke(new SingleFileManagerEventArgs($"{name} has been deleted", name, null));
                 }
                 else
                 {
-                    Console.WriteLine();
+                    Console.WriteLine("Sorry! There's no such file");
                 }
             }
 
         }
+
+        class SingleFileManagerEventArgs
+        {
+            public string Message { get; }
+            public string FileName { get; }
+            public string AppendData { get; }
+            public SingleFileManagerEventArgs(string message, string fileName, string appendData)
+            {
+                Message = message;
+                FileName = fileName;
+                AppendData = appendData;
+            }
+        }
+
         public static void Main()
         {
             var fileManager = new SingleFileManager("newfile.txt");
 
-            /*fileManager.OnFileCreated += x => Console.WriteLine($"Файл {x.FileName} создан");
+            fileManager.OnFileCreated += x => Console.WriteLine($"Файл {x.FileName} создан");
             fileManager.OnFileDeleted += x => Console.WriteLine($"Файл {x.FileName} удален");
-            fileManager.OnFileModified += x => Console.WriteLine($"В файл {x.FileName} была добавлено {x.AppendData}");*/
+            fileManager.OnFileModified += x => Console.WriteLine($"В файл {x.FileName} была добавлено {x.AppendData}");
 
             fileManager.Create();
             fileManager.AppendData("Новая инфа");
-            //fileManager.Delete();
+            fileManager.Delete();
         }
     }
 }
