f31430b0293c0e8a24915e44452a201c21c903c2
Galina Amelina <galia.amelina@yandex.ru>
2022-07-31 22:54:40 +0300

    Add EventHandler

--- задача на события/Program.cs
+++ задача на события/Program.cs
@@ -1,81 +1,37 @@
-﻿class Program
+﻿namespace eventTask
 {
-
-    public class EventUsing
+    class Program
     {
-        class SingleFileManager
-        {
-            private string _fileName;
-            public SingleFileManager(string fileName) => this._fileName = fileName;
-
-            public delegate void FileHandler(SingleFileManagerEventArgs e);
-            public event FileHandler? OnFileCreated;
-            public event FileHandler? OnFileDeleted;
-            public event FileHandler? OnFileModified;
-
-
-            public void Create()
-            {
-                using (var fstream = new FileStream(_fileName, FileMode.OpenOrCreate))
-                {
-                    OnFileCreated?.Invoke(new SingleFileManagerEventArgs($"{_fileName} has been created", _fileName, null));
-                }
-            }
-            public void AppendData(string data)
-            {
-
-                if (File.Exists(_fileName))
-                {
-                    using (var writer = new StreamWriter(_fileName, true))
-                    {
-                        writer.WriteLine(data);
-                        OnFileModified?.Invoke(new SingleFileManagerEventArgs($"{_fileName} has been changed", _fileName, data));
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"There's no file named {_fileName} to add new information");
-                }
-            }
-            public void Delete()
-            {
-                if (File.Exists(_fileName))
-                {
-                    File.Delete(_fileName);
-                    OnFileDeleted?.Invoke(new SingleFileManagerEventArgs($"{_fileName} has been deleted", _fileName, null));
-                }
-                else
-                {
-                    Console.WriteLine("Sorry! There's no such file");
-                }
-            }
-
-        }
-
-        class SingleFileManagerEventArgs
-        {
-            public string Message { get; }
-            public string FileName { get; }
-            public string AppendData { get; }
-            public SingleFileManagerEventArgs(string message, string fileName, string appendData)
-            {
-                Message = message;
-                FileName = fileName;
-                AppendData = appendData;
-            }
-        }
-
         public static void Main()
         {
             var fileManager = new SingleFileManager("newfile.txt");
 
-            fileManager.OnFileCreated += x => Console.WriteLine($"Файл {x.FileName} создан");
-            fileManager.OnFileDeleted += x => Console.WriteLine($"Файл {x.FileName} удален");
-            fileManager.OnFileModified += x => Console.WriteLine($"В файл {x.FileName} была добавлено {x.AppendData}");
+            var eventOnCreated = new EventHandler<SingleFileManagerEventArgs>(FileCreated);
+            var eventOnDeleted = new EventHandler<SingleFileManagerEventArgs>(FileDeleted);
+            var eventOnModified = new EventHandler<SingleFileManagerEventArgs>(FileModified);
+
+            fileManager.OnFileCreated += eventOnCreated;
+            fileManager.OnFileDeleted += eventOnDeleted;
+            fileManager.OnFileModified += eventOnModified;
 
             fileManager.Create();
             fileManager.AppendData("Новая инфа");
             fileManager.Delete();
+
+
+
+        }
+        public static void FileCreated(object sender, SingleFileManagerEventArgs e)
+        {
+            Console.WriteLine($"Файл {e.FileName} создан");
+        }
+        public static void FileDeleted(object sender, SingleFileManagerEventArgs e)
+        {
+            Console.WriteLine($"Файл {e.FileName} удален");
+        }
+        public static void FileModified(object sender, SingleFileManagerEventArgs e)
+        {
+            Console.WriteLine($"В файл {e.FileName} была добавлено {e.AppendData}");
         }
     }
 }
---
+++ задача на события/SingleFileManager.cs
@@ -0,0 +1,51 @@
+﻿
+namespace eventTask
+{
+    class SingleFileManager
+    {
+        public string _fileName;
+        public SingleFileManager(string fileName) => this._fileName = fileName;
+
+        public event EventHandler<SingleFileManagerEventArgs> OnFileCreated;
+        public event EventHandler<SingleFileManagerEventArgs> OnFileDeleted;
+        public event EventHandler<SingleFileManagerEventArgs> OnFileModified;
+
+
+        public void Create()
+        {
+            using (var fstream = new FileStream(_fileName, FileMode.OpenOrCreate))
+            {
+                OnFileCreated?.Invoke(this, new SingleFileManagerEventArgs { Message = $"{_fileName} has been created", FileName = _fileName });
+            }
+        }
+        public void AppendData(string data)
+        {
+             
+            if (File.Exists(_fileName))
+            {
+                using (var writer = new StreamWriter(_fileName, true))
+                {
+                    writer.WriteLine(data);
+                    OnFileModified?.Invoke(this, new SingleFileManagerEventArgs { Message = $"{_fileName} has been changed", FileName = _fileName, AppendData = data });
+                }
+            }
+            else
+            {
+                Console.WriteLine($"There's no file named {_fileName} to add new information");
+            }
+        }
+        public void Delete()
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+                OnFileDeleted?.Invoke(this, new SingleFileManagerEventArgs { Message = $"{_fileName} has been deleted", FileName = _fileName });
+            }
+            else
+            {
+                Console.WriteLine("Sorry! There's no such file");
+            }
+        }
+
+    }
+}
